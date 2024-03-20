55865e813e308c53dce766b7fc7ec0b52d4e50fc
Stan Racz <stan.racz7@gmail.com>
2024-03-20 22:32:53 +0100

    MMCU-3 : project added to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'bloc/add_manga_bloc/add_manga_bloc.dart';
 import 'locator.dart';
 import 'view/add_manga.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   setupLocator();
   runApp(const MangaChapterUpdateApp());
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MangaChapterUpdateApp extends StatelessWidget {
